d39280e128691c6771c587337f892b06a284c92c
wadhwaravi <117942469+wadhwaravi@users.noreply.github.com>
2025-01-18 00:09:34 +0530

    login backend

--- vite-project/src/pages/registrations/Login.jsx
+++ vite-project/src/pages/registrations/Login.jsx
@@ -1,6 +1,32 @@
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import myContext from "../../context/data/myContext";
+import { auth } from "../../firebase/FireBaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { toast } from "react-toastify";
+import Loader from "../../components/loader/Loader";
 function Login() {
+  const context = useContext(myContext);
+  const { loading, setLoading } = context;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const login = async () => {
+    {
+      loading && <Loader />;
+    }
+    try {
+      setLoading(true);
+      const result = await signInWithEmailAndPassword(auth, email, password);
+      toast.success("Login successfull");
+      localStorage.setItem("user", JSON.stringify(result));
+      navigate("/");
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
   return (
     <div className=" flex justify-center items-center h-screen">
       <div className=" bg-gray-800 px-10 py-10 rounded-xl ">
@@ -13,6 +39,8 @@ function Login() {
           <input
             type="email"
             name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
             placeholder="Email"
           />
@@ -20,12 +48,17 @@ function Login() {
         <div>
           <input
             type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
             placeholder="Password"
           />
         </div>
         <div className=" flex justify-center mb-3">
-          <button className=" bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg">
+          <button
+            className=" bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg"
+            onClick={login}
+          >
             Login
           </button>
         </div>
